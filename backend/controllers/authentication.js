1d7d0e2f3340a1799e20ddd2b902a005c81f1c91
Joey <joeyhoellerich@gmail.com>
2022-09-06 12:55:12 -0400

    added jwt authentication

--- backend/controllers/authentication.js
+++ backend/controllers/authentication.js
@@ -1,20 +1,45 @@
 const router = require('express').Router()
 const db = require("../models")
 const bcrypt = require('bcrypt')
-
+const jwt = require("jsonwebtoken")
 const { User } = db
 
 router.post('/', async (req, res) => {
     let user = await User.findOne({
         where: { email: req.body.email }
     })
-    console.log(user)
+
     if (!user || !await bcrypt.compare(req.body.password, user.passwordDigest)) {
         res.status(404).json({
             message: "could not find user"
         })
     } else {
-        res.json( { user })
+        //const result = await jwt.encode(process.env.JWT_SECRET, { id: user.userId })
+        const token = jwt.sign({id: user.userId}, process.env.JWT_SECRET)
+        //req.session.userId = user.userId
+        res.json( { user: user, token: token} )
+    }
+})
+
+router.get('/profile', async (req, res) => {
+    try {
+        //Split authorization header into ["Bearer", "Token"]
+        const [authenticationMethod, token] = req.headers.authorization.split(' ')
+
+        if (authenticationMethod == "Bearer"){
+            const result = jwt.verify(token, process.env.JWT_SECRET)
+
+            const { id } = result
+            
+            let user = await User.findOne({
+                where: {
+                    userId: id
+                }
+            })
+            res.json(user)
+        }
+    } catch {
+        res.json(null)
     }
 })
 
